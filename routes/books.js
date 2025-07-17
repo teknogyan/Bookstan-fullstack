7706c0f0d3049d5463290d3403edbbe5aba55b54
tekno <teknogyan123@gmail.com>
2025-07-17 13:07:46 +0530

    all CRUD routes for books finished

--- routes/books.js
+++ routes/books.js
@@ -5,7 +5,7 @@ console.log(Books);
 const Author = require("../models/author");
 const mongoose = require("mongoose");
 
- // allowed images files formats for upload
+// allowed images files formats for upload
 // const imageMimeTypes = ["image/jpeg", "image/png", "image/gif"];
 
 // const upload = multer({
@@ -30,8 +30,6 @@ router.get("/", async (req, res) => {
   res.render("books", { books: books, searchQuery, error: null });
 });
 
-
-
 // Add New Book Route
 router.get("/new", async (req, res) => {
   try {
@@ -41,24 +39,19 @@ router.get("/new", async (req, res) => {
   } catch (err) {
     res.redirect("/books");
   }
-
 });
 
 // GET Route to load individual book
-router.get("/:id", async(req, res) => {
+router.get("/:id", async (req, res) => {
   try {
     const bookId = req.params.id;
-  console.log("book id requested", bookId)
-  const book = await Books.findById(bookId).populate("author");
-  console.log(book)
-  res.render("books/view", {book})
-    
-  } catch (error) {
-    
-  }
+    console.log("book id requested", bookId);
+    const book = await Books.findById(bookId).populate("author");
+    console.log(book);
+    res.render("books/view", { book });
+  } catch (error) {}
   // TODO: figure out how to create and render dynamic views according to 'id'
-
-})
+});
 
 // Route for creating new book
 router.post("/", async (req, res) => {
@@ -67,13 +60,11 @@ router.post("/", async (req, res) => {
     author: authorId, // destructering the variable author and setting it as authorId
     published,
     pages,
-    created, 
+    created,
     thumbnail,
     description,
   } = req.body;
   const authorObj = await Author.findById(authorId);
-
-  console.log( title, authorObj, authorId, pages, published, created, thumbnail, description);
 
   const book = new Books({
     title: title,
@@ -96,8 +87,71 @@ router.post("/", async (req, res) => {
   }
 });
 
+//Route for serving edit page
+router.get("/edit/:id", async (req, res) => {
+  try {
+    const bookId = req.params.id;
+    console.log("book id requested", bookId);
+    const book = await Books.findById(bookId).populate("author");
+    const authors = await Author.find({});
+    console.log(book.title);
+    res.render("books/edit", { book, authors });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-router.post("/delete", async (req, res) => {
+// Rout for updating the book info
+router.put("/", async (req, res) => {
+  console.log("From put route:", req.body);
+
+  const {
+    id,
+    title,
+    author: authorId, // destructering the variable author and setting it as authorId
+    published,
+    pages,
+    created,
+    thumbnail,
+    description,
+  } = req.body;
+  const authorObj = await Author.findById(authorId);
+
+  console.log(
+    id,
+    title,
+    authorObj,
+    authorId,
+    pages,
+    published,
+    created,
+    thumbnail,
+    description
+  );
+
+  try {
+    await Books.findByIdAndUpdate(
+      id,
+      {
+        title: title,
+        publishedDate: new Date(published),
+        pageCount: pages,
+        createdAt: new Date(created),
+        author: authorObj,
+        description: description,
+        thumbnail: thumbnail,
+      }
+    );
+    res.redirect("/books")
+  } catch (error) {
+    console.log("error while updating: ", error);
+    const book = await Books.findById(id);
+    const authors = await Author.find({});
+    res.render("authors/edit", {book, authors })
+  }
+});
+
+router.delete("/", async (req, res) => {
   console.log(req.body.id);
   const bookId = req.body.id;
   try {
@@ -112,7 +166,6 @@ router.post("/delete", async (req, res) => {
     });
   }
 });
-
 
 // Utility functions
 // To load books from db and populate authors
